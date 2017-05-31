e0ecfb45fa73c70bb11cfc0dbc16bf9da3e7b5fc
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-05-31 08:24:17 +0100

    replaced ansible with bash script

---
+++ provision.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone -b final https://github.com/thecodingbrain/bookiemarker-frontend.git /vagrant/frontend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-backend.git /vagrant/backend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-worker.git /vagrant/worker
