c1484bc201d7ecc594ec83e423e66a18fd28f242
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-05-31 17:04:08 +0100

    changed line endings

--- provision.bat
+++ provision.bat
@@ -1,3 +1,3 @@
-git clone -b final https://github.com/thecodingbrain/bookiemarker-frontend.git frontend
-git clone -b final https://github.com/thecodingbrain/bookiemarker-backend.git backend
-git clone -b final https://github.com/thecodingbrain/bookiemarker-worker.git worker
+git clone -b final https://github.com/thecodingbrain/bookiemarker-frontend.git /vagrant/frontend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-backend.git /vagrant/backend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-worker.git /vagrant/worker
