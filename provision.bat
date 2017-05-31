3f4493f86f5cbac5fde4b6867810e00d488ae6b0
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-05-31 11:09:32 +0100

    added windows batch

--- provision.bat
+++ provision.bat
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-git clone -b final https://github.com/thecodingbrain/bookiemarker-frontend.git /vagrant/frontend
-git clone -b final https://github.com/thecodingbrain/bookiemarker-backend.git /vagrant/backend
-git clone -b final https://github.com/thecodingbrain/bookiemarker-worker.git /vagrant/worker
+git clone -b final https://github.com/thecodingbrain/bookiemarker-frontend.git frontend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-backend.git backend
+git clone -b final https://github.com/thecodingbrain/bookiemarker-worker.git worker
